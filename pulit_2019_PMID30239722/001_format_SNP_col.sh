67aa985788bac25eb7f38f6356540c29b0ffb527
Matthew Lee <leem@hn.pioneerx>
2023-06-07 14:53:52 +0200

    update

--- pulit_2019_PMID30239722/001_format_SNP_col.sh
+++ pulit_2019_PMID30239722/001_format_SNP_col.sh
@@ -1,6 +1,6 @@
 # format SNP column
-cd ~/001_projects/000_datasets/adiposity_GWAS/pulit_EU_2018/raw/
-
+cd /data/GWAS_data/files/pulit_2018_PMID30239722/raw/
+  
 awk '{sub(/:.*/,"",$3)} 1' bmi.giant-ukbb.meta-analysis.combined.23May2018.txt > ../processed/bmi_combined.txt
 awk '{sub(/:.*/,"",$3)} 1' bmi.giant-ukbb.meta-analysis.females.23May2018.txt > ../processed/bmi_female.txt
 awk '{sub(/:.*/,"",$3)} 1' bmi.giant-ukbb.meta-analysis.males.23May2018.txt > ../processed/bmi_male.txt
@@ -10,12 +10,3 @@ awk '{sub(/:.*/,"",$3)} 1' whr.giant-ukbb.meta-analysis.males.23May2018.txt > ..
 awk '{sub(/:.*/,"",$3)} 1' whradjbmi.giant-ukbb.meta-analysis.combined.23May2018.txt > ../processed/whradjbmi_combined.txt
 awk '{sub(/:.*/,"",$3)} 1' whradjbmi.giant-ukbb.meta-analysis.females.23May2018.txt > ../processed/whradjbmi_female.txt
 awk '{sub(/:.*/,"",$3)} 1' whradjbmi.giant-ukbb.meta-analysis.males.23May2018.txt > ../processed/whradjbmi_male.txt
-
-# add phenotype col of filename to adiposity gwas
-cd ~/001_projects/000_datasets/adiposity_GWAS/pulit_EU_2018/processed/
-
-tmp=$(mktemp) || { ret="$?"; printf 'Failed to create temp file\n'; exit "$ret"; }
-for file in *.txt; do
-    awk 'BEGIN{OFS="\t"} {print $0, (FNR>1 ? FILENAME : "phenotype")}' "$file" > "$tmp" &&
-    mv -- "$tmp" "$file" || exit
-done
