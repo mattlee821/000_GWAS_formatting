664b3259a065cc8e6c5115b9c39037eff444b18b
Matthew Lee <leem@hn.pioneerx>
2023-11-07 13:45:13 +0100

    rm deprecated

--- CRC_early-onset/002_phenotype_col.sh
+++
@@ -1,8 +0,0 @@
-# add phenotype col to crc gwas
-cd ~/001_projects/000_datasets/CRC_early-onset/processed
-# make phenotype column
-tmp=$(mktemp) || { ret="$?"; printf 'Failed to create temp file\n'; exit "$ret"; }
-for file in *annotated.txt; do
-awk 'BEGIN{OFS="\t"} {print $0, (FNR>1 ? FILENAME : "phenotype")}' "$file" > "$tmp" &&
-  mv -- "$tmp" "$file" || exit
-done
--- pulit_2019_PMID30239722/002_phenotype_col.sh
+++
@@ -1,8 +0,0 @@
-# add phenotype col of filename to adiposity gwas
-cd /data/GWAS_data/files/pulit_2018_PMID30239722/processed
-
-tmp=$(mktemp) || { ret="$?"; printf 'Failed to create temp file\n'; exit "$ret"; }
-for file in *.txt; do
-    awk 'BEGIN{OFS=" "} {print $0, (FNR>1 ? FILENAME : "phenotype")}' "$file" > "$tmp" &&
-    mv -- "$tmp" "$file" || exit
-done
