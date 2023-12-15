7dd3bd96ab56fb2ace759ec985f0c8b0adedf330
matt <matthewlee821@gmailcom>
2023-12-15 12:28:56 +0100

    add

---
+++ ferkingstad_2021_PMID34857953/000_move_data/filelist17.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=mv-filelist17
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=20-1:0:00
+#SBATCH --mem=100000M
+
+cd /data/protein_GWAS_ferkingstad_EU_2021/files/processed/
+
+for file in $(cat filelist17); do
+    cp "$file" "/data/protein_GWAS_ferkingstad_EU_2021/files/processed/"
+done
