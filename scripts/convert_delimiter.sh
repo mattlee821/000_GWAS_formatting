c9241ae11817885ff9aa19718c4935ec1ddbe5e5
matt <matthewlee821@gmailcom>
2024-11-30 16:10:30 +0100

    clean up

--- scripts/convert_delimiter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Convert comma-separated to tab-separated
-perl -wnlpi -e 's/,/\t/g;' file
-
-# Convert space-separated (i.e., more than one space) to tab-separated
-perl -wnlpi -e 's/\s+/\t/g;' file
--- scripts/phenotype_col.sh
+++
@@ -1,7 +0,0 @@
-# add phenotype col of filename to gwas
-
-tmp=$(mktemp) || { ret="$?"; printf 'Failed to create temp file\n'; exit "$ret"; }
-for file in *.txt; do
-awk 'BEGIN{OFS=" "} {print $0, (FNR>1 ? FILENAME : "phenotype")}' "$file" > "$tmp" &&
-  mv -- "$tmp" "$file" || exit
-done
