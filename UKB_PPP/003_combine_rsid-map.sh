60cb51fb2f905dc2f2940963391bdd278609e5ec
matt <matthewlee821@gmailcom>
2024-12-02 17:25:15 +0100

    clean up

--- UKB_PPP/003_combine_rsid-map.sh
+++
@@ -1,3 +0,0 @@
-# map rsid
-cat olink_rsid_map_mac5_info03_b0_7_chr1_patched_v2.tsv | head -n 1 > combined.txt
-cat olink* | grep -v '^ID' >> combined.txt
--- UKB_PPP/005_cis-snps/001_filelist.sh
+++
@@ -1,3 +0,0 @@
-# make list of protein files
-cd /data/GWAS_data/work/UKB_PPP/
-ls /data/GWAS_data/work/UKB_PPP/european/*gz > /data/GWAS_data/work/UKB_PPP/cis-snps/001_files.txt
